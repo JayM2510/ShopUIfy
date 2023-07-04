1e569e992bb6760b2ff74d96ee3f8a5256eb7781
JayM2510 <jay.mishra2510@gmail.com>
2023-07-04 22:48:47 +0530

    Intializing firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:shop_app/routes.dart';
 import 'package:shop_app/screens/profile/profile_screen.dart';
 import 'package:shop_app/screens/splash/splash_screen.dart';
 import 'package:shop_app/theme.dart';
 
-void main() {
+void main() async{
+   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(); // Initialize Firebase
   runApp(MyApp());
 }
 
